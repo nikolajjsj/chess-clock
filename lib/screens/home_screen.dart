9c3b26e1f471b3dcdf6c825a185a8e0f786fa7dd
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2020-12-26 12:46:36 +0100

    Updated with route to settings screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,20 @@
+import 'package:chessclock/screens/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chess Clock')),
+      appBar: AppBar(
+        title: Text('Chess Clock'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () => Navigator.of(context).push(SettingsScreen.route()),
+          )
+        ],
+      ),
       body: Center(child: Text('Chess!')),
     );
   }
