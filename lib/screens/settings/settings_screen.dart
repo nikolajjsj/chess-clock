e0fc2a1c154bbd0801839853985e59256c7810ef
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2020-12-27 09:59:51 +0100

    Implemented theme options

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,16 +1,47 @@
+import 'package:chessclock/misc/preferences/layout_preferences.dart';
+import 'package:chessclock/misc/service_locator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static Route route() {
     return CupertinoPageRoute(builder: (context) => SettingsScreen());
   }
 
   @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
   Widget build(BuildContext context) {
+    final _followSystem = app<LayoutPreferences>().followSystemTheme;
+    final _lightTheme = app<LayoutPreferences>().lightTheme;
+
     return Scaffold(
       appBar: AppBar(title: Text('Settings'), centerTitle: true),
-      body: Container(),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Text('Follow system theme'),
+            value: _followSystem,
+            onChanged: (val) {
+              app<LayoutPreferences>().followSystemTheme = val;
+              setState(() {});
+            },
+          ),
+          SwitchListTile(
+            title: Text('Light theme'),
+            value: _lightTheme,
+            onChanged: _followSystem
+                ? null
+                : (val) {
+                    app<LayoutPreferences>().lightTheme = val;
+                    setState(() {});
+                  },
+          ),
+        ],
+      ),
     );
   }
 }
