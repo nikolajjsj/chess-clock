e7fa36ce2228b52c6be9b809722aef7399dca1bd
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2021-01-06 10:40:18 +0100

    Removed compact layout (nicer & simpler)

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,7 +1,4 @@
-import 'package:chessclock/misc/message_service/message_service.dart';
 import 'package:chessclock/misc/preferences/bloc/theme_bloc.dart';
-import 'package:chessclock/misc/preferences/preferences.dart';
-import 'package:chessclock/misc/service_locator.dart';
 import 'package:chessclock/widgets/settings/settings_header.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,22 +16,10 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    final compact = app<AppPreferences>().compact;
-
     return Scaffold(
       appBar: AppBar(title: Text('Settings'), centerTitle: true),
       body: ListView(
         children: [
-          SettingsHeader(title: 'Layout'),
-          SwitchListTile(
-            title: Text('Compact clock cards'),
-            value: compact,
-            onChanged: (val) {
-              app<AppPreferences>().compact = val;
-              setState(() {});
-              app<MessageService>().show('Please restart app');
-            },
-          ),
           SettingsHeader(title: 'Theming'),
           BlocBuilder<ThemeBloc, ThemeState>(
             builder: (context, state) {
